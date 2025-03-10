0a90ee2f838f59b9531c037d443782bba66af87a
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2025-03-10 12:26:30 +0300

    Update task9.sh

--- Lab2/src/task/task9.sh
+++ Lab2/src/task/task9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-automount="UUID=$(blkid -s UUID -o vakue /dev/sdb1) /mnt/newdisk ext4 noexec,noatime 0 2"
+automount="UUID=$(blkid -s UUID -o value /dev/sdb1) /mnt/newdisk ext4 noexec,noatime 0 2"
 echo $automount >> /etc/fstab
 
 reboot
